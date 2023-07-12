048692231bf3c3c54773505817fda1130ca07695
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2023-07-12 15:15:42 +0300

    task-03+

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const LOCAL_KEY = 'feedback-form-state';
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+let formData = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+
+setFormData();
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
+}
+
+function setFormData() {
+  const savedFormData = JSON.parse(localStorage.getItem(LOCAL_KEY));
+  if (savedFormData) {
+    form.email.value = savedFormData.email;
+    form.message.value = savedFormData.message;
+  }
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  console.log(formData);
+
+  localStorage.removeItem(LOCAL_KEY);
+  event.currentTarget.reset();
+}
